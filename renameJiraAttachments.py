fa942afeb188aeb869c8fb552a3a48b5ff581b83
itskalai <itskalai@gmail.com>
2020-04-21 19:56:52 -0700

    Create renameJiraAttachments.py
    Rename  Jira Attachments from the ID names to the actual file names so it can be imported in another instance

---
+++ renameJiraAttachments.py
@@ -0,0 +1,18 @@
+#Rename the Jira Attachments from the ID names to the actual file names so it can be imported in another instance
+import os
+filenames = {}
+absfilenames = {}
+#top is the root of the attachments folder
+top="/Users/kalaivani_ramasamy/importAttachments"
+#list.txt contains the output of the query :
+# select fa.id, fa.filename, ji.issuenum, p.pkey from fileattachment fa join jiraissue ji on ji.id= fa.issueid join project p on p.id = ji.project WHERE p.pkey = 'THIS;
+with open("list.txt") as fh:
+    for line in fh:
+        if line.startswith("-"):
+            continue
+        else:
+            id, file, ignore = line.split("|",2)
+            for root, dirs, files in os.walk(top, topdown=False):
+                for name in files:
+                    if name in file.strip():
+                        os.rename(os.path.join(root,name),os.path.join(root,id.strip()))
